b420209623ea177d4fd5ccb2b689fbbbc2f385fc
unknown <ghasedikobra@gmail.com>
2023-12-27 16:31:24 +0330

    salam1

--- src/components/ShowProductDetails.jsx
+++ src/components/ShowProductDetails.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import './ShowProductDetails.css'
+import styles from './ShowProductDetails.module.css'
 const ShowProductDetails = ({data}) => {
   
   return (
     <div>
-        <div className='dvContainerDetails'>
-          <div className='dvimageDetails'><img src={data.indexImageUrl} alt="" /></div>
-          <div className='dvProductNameDetails'>
-            <p>{data.productName}</p>
+        <div className={`${styles.dvContainerDetails}`}>
+          <div className={`${styles.dvimageDetails}`}><img src={data.indexImageUrl} alt="" /></div>
+          <div className={`${styles.dvProductNameDetails}`}>
+            <p className={`${styles.Name}`}>{data.productName}</p>
             <span>توضیحات:</span>
-            <p>{data.decription}</p>
-            <span className='spnPriceWithDiscount'>{data.priceWithDiscount}</span>
+            <p className={`${styles.Description}`}>{data.description}</p>
+            <span className={`${styles.spnPriceWithDiscount}`}>{data.priceWithDiscount}</span>
             <span>تومان</span>  
-            <span className='spnPrice'>{data.price}</span>
+            <span className={`${styles.spnPrice}`}>{data.price}</span>
             <span>تومان</span>        
          </div>
       </div>
